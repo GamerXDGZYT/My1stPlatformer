f109eb19e22b25da9266e84c778872410bc4570e
GamerXDGZYT <singhgitansh741@gmail.com>
2026-05-07 17:05:39 -0700

    Update Sprite.js

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,1 +1,13 @@
+export class Sprite {
+    constructor({ position, color = 'red', width = 50, height = 50 }) {
+        this.position = position;
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
 
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
